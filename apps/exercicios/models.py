f0b04a383167128d7c12b9b836fb4e79be6d77e6
MiguelMacena <miguelmacena1610@gmail.com>
2025-12-31 00:29:12 -0300

    feat(exercicios): cria model e service para integração com ExerciseDB

--- apps/exercicios/models.py
+++ apps/exercicios/models.py
@@ -1,3 +1,58 @@
-# from django.db import models
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Exercicio(models.Model):
+    # cria parâmetros do exercicio que será recebido da API exeterna
+    external_id = models.CharField(
+        max_length=50,
+        unique=True,
+        verbose_name=_("ID Externo"),
+        help_text=_("ID do exercicio no ExerciseDB"),
+    )
+
+    name = models.CharField(
+        max_length=255,
+        verbose_name=_("Nome do Exercicio"),
+    )
+
+    target = models.CharField(
+        max_length=100,
+        verbose_name=_("Músculo Alvo"),
+    )
+
+    secondary_muscles = models.JSONField(
+        default=list, verbose_name=_("Musculos Secundários")
+    )
+
+    body_part = models.CharField(max_length=100, verbose_name=_("Parte do corpo"))
+
+    equipment = models.CharField(
+        max_length=100,
+        verbose_name=_("Equipamento"),
+    )
+
+    category = models.CharField(max_length=50, verbose_name=_("Categoria"))
+
+    difficulty = models.CharField(max_length=50, verbose_name=_("Dificuldade"))
+
+    instructions = models.JSONField(default=list, verbose_name=_("Instruções"))
+
+    description = models.TextField(verbose_name=_("Descrição"))
+
+    gif_url = models.URLField(verbose_name=_("Gif do Exercicio"))
+
+    criado_em = models.DateTimeField(auto_now=True, verbose_name=_("Criado em"))
+
+    atualizado_em = models.DateField(auto_now=True, verbose_name=_("Atualizado em"))
+
+    class Meta:
+        verbose_name = _("Exercício")
+        verbose_name_plural = _("Exercícios")
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
 
 # Create your models here.
--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-secret-key")
 
+EXERCISE_DB_API_KEY = "a7eae126ebmsh9c3429169217682p19593ejsn6d7dc1af94ae"
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
